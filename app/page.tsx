dfa31acfd3b72603b17b99bb8ad8bd2f510c058f
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-18 16:47:21 -0800

    fixed issue with navbar width

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between max-w-7xl">
+    <>
+    <main className="flex min-h-screen flex-col justify-between">
       <NavBar />
       <div className="flex flex-col items-center justify-between p-60">
         <p>Test</p>
       </div>
     </main>
+    </>
   );
 }
