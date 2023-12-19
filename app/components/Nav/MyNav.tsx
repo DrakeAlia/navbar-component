b1b76f894e2d9152a09e6b2e5177d43912301560
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-12-19 10:04:16 -0800

    created a second navbar to play around with

--- app/components/Nav/MyNav.tsx
+++ app/components/Nav/MyNav.tsx
@@ -17,26 +17,30 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function NavBar() {
+export default function MyNav() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
+          {/* Main Container */}
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            {/* Inner Container */}
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                {/* Mobile Menu Button Container*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    <Bars3Icon className="block h-6 w-6 " aria-hidden="true" />
                   )}
                 </Disclosure.Button>
               </div>
+              {/* Logo Container */}
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                {/* Logo Inner Container */}
                 <div className="flex flex-shrink-0 items-center">
                   {/* Icon Image */}
                   <img
@@ -45,7 +49,9 @@ export default function NavBar() {
                     alt="Your Company"
                   />
                 </div>
+                {/* Navbar Items Container */}
                 <div className="hidden sm:ml-6 sm:block">
+                  {/* Navbar Items Inner Container */}
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <Link
@@ -65,6 +71,7 @@ export default function NavBar() {
                   </div>
                 </div>
               </div>
+              {/* Bell Icon */}
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
@@ -74,8 +81,7 @@ export default function NavBar() {
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-
-                {/* Profile dropdown */}
+                {/* Profile Container */}
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -98,6 +104,7 @@ export default function NavBar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
+                    {/* Profile Dropdown Menu Container */}
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
-import NavBar from "@/components/NavBar";
+import NavBar from "@/app/components/Nav/NavBar";
 import Image from "next/image";
+import MyNav from "./components/Nav/MyNav";
 
 export default function Home() {
   return (
-    <>
-      <main className="flex min-h-screen flex-col justify-between">
+    // Main Container
+    <main className="text-center items-center min-h-screen mx-auto">
+      {/* Navbar Container */}
+      <div className="flex flex-col gap-8">
         <NavBar />
-        <div className="flex flex-col items-center justify-between p-60">
-          <p>Test</p>
+        <MyNav />
+      </div>
+      {/* Hero Container */}
+      <div className="flex flex-row items-center p-40">
+        <div className="flex-row mx-auto max-w-2xl">
+          <h1 className="text-4xl font-bold sm:text-6xl">Test</h1>
+          <p className="mt-6 text-lg">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            inventore, voluptatum, voluptates, doloremque quos quas voluptatem
+            quae quia doloribus officia consequuntur. Quisquam inventore,
+            voluptatum, voluptates, doloremque quos quas voluptatem quae quia
+            doloribus officia consequuntur.
+          </p>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
